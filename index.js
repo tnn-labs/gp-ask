28cebee74dc48b1ebc9a125b4942004b278573ae
theandersonn <theandersonn@gmail.com>
2022-04-16 14:14:23 -0300

    capturando dados de um formulario

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // configuração para o express utilizar o EJS como view engine
 app.set("view engine", "ejs");
-app.use(express.static('public'))
+app.use(express.static('public'));
 
+// body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// rota
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -14,7 +20,9 @@ app.get("/perguntar", (req, res) => {
 });
 
 app.post("/salvarpergunta", (req, res) => {
-  res.send("Formulário recebido")
+  const titulo = req.body.titulo;
+  const descricao = req.body.descricao;
+  res.send(`Formulário recebido: titulo: ${titulo} - descrição: ${descricao}`)
 })
 
 app.listen(8080, () => {
